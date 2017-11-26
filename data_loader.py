abdf03afc2fe37dd1ff67d40be77004b4a8dbb52
timatim <timhsu1994@gmail.com>
2017-11-25 22:39:24 -0500

    updated data loader for lazier embedding

--- data_loader.py
+++ data_loader.py
@@ -4,36 +4,34 @@ from torch.utils.data import Dataset
 import utils
 
 
-class QuoraDatum():
+class QuoraDataset(Dataset):
     def __init__(self, p, q, label, words, chars, seq_len=50, word_len=20, cuda=False):
         self.p = p
         self.q = q
-        self.p_words, self.p_chars = utils.sentence_to_padded_index_sequence(p, words, chars,
-                                                                             seq_len=seq_len, word_len=word_len,
-                                                                             cuda=cuda)
-        self.q_words, self.q_chars = utils.sentence_to_padded_index_sequence(q, words, chars,
-                                                                             seq_len=seq_len, word_len=word_len,
-                                                                             cuda=cuda)
         self.label = label
-
-
-class QuoraDataset(Dataset):
-    def __init__(self, data):
-        self.data = data
+        self.words = words
+        self.chars = chars
+        self.seq_len = seq_len
+        self.word_len = word_len
+        self.cuda = cuda
 
     def __len__(self):
-        return len(self.data)
+        return len(self.label)
 
     def __getitem__(self, key):
-        d = self.data[key]
+        p_words, p_chars = utils.sentence_to_padded_index_sequence(
+            self.p[key], self.words, self.chars,
+            seq_len=self.seq_len, word_len=self.word_len, cuda=self.cuda)
 
-        return (d.p_words, d.p_chars, d.q_words, d.q_chars), d.label
+        q_words, q_chars = utils.sentence_to_padded_index_sequence(
+            self.q[key], self.words, self.chars,
+            seq_len=self.seq_len, word_len=self.word_len, cuda=self.cuda)
+
+        return (p_words, p_chars, q_words, q_chars), self.label[key]
 
 
 def make_dataloader(df, words, chars, seq_len=50, word_len=20, batch_size=128, shuffle=True, cuda=False):
-    dataset = QuoraDataset([QuoraDatum(row['p'], row['q'], row['label'], words, chars,
-                                       seq_len=seq_len, word_len=word_len, cuda=cuda)
-                            for (_, row) in df.iterrows()])
+    dataset = QuoraDataset(df['p'], df['q'], df['label'], words, chars, seq_len=seq_len, word_len=word_len, cuda=cuda)
     train_loader = torch.utils.data.DataLoader(dataset=dataset,
                                                batch_size=batch_size,
                                                shuffle=shuffle)
